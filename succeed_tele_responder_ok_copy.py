09b57f1dbae1d2931f38d0a1019c55a0b8d1e419
jinhgoh <68317603+jinhgoh@users.noreply.github.com>
2020-07-15 14:53:34 +0900

    Add files via upload

---
+++ succeed_tele_responder_ok_copy.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import telepot
+
+token = '1161290144:AAFmL2S19FhgaQYzsD0QiovTSpW6E21tzxo'
+mc = '1141765178'
+bot = telepot.Bot(token)
+
+def handle(msg):
+    with urlopen('https://www.jbnu.ac.kr/kor/?menuID=139&pno=1') as response:
+        soup = BeautifulSoup(response, 'html.parser')
+        i = 1
+        for anchor in soup.select('a[href^="?menuID=139&pno=1&mode=view&no="]'):
+            announcement = anchor.get_text(strip=True)
+            if not announcement.find(msg['text']) == -1:
+                data = '[' + str(i) + '] ' + announcement
+                print(data)
+                bot = telepot.Bot('1161290144:AAFmL2S19FhgaQYzsD0QiovTSpW6E21tzxo')
+                bot.sendMessage(msg['from']['id'], data)
+                i = i + 1
+bot.message_loop(handle)
+
+while True:
+    pass
+
+"""
+
+with urlopen('https://www.jbnu.ac.kr/kor/?menuID=139&pno=1') as response:
+    soup = BeautifulSoup(response, 'html.parser')
+    i = 1
+    for anchor in soup.select('a[href^="?menuID=139&pno=1&mode=view&no="]'):
+        announcement = anchor.get_text()
+        if not announcement.find('장학') == -1:
+            data = str(i) + announcement
+            print(data)
+            bot = telepot.Bot('1161290144:AAFmL2S19FhgaQYzsD0QiovTSpW6E21tzxo')
+            bot.sendMessage(1141765178, data)
+            i = i + 1
+"""
